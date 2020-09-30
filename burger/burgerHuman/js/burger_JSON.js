f7d0e73db42d9d878e5ca2a243d3069babbea789
ddow7 <34911482+ddow7@users.noreply.github.com>
2020-09-30 16:23:02 +0900

    Add files via upload
    가상 수제버거 전문점 웹페이지 업로드

---
+++ burger/burgerHuman/js/burger_JSON.js
@@ -0,0 +1,191 @@
+eqfeed_callback({
+    "type": "FeatureCollection",
+    "features": [{
+        "type": "Feature",
+        "properties": {
+			"name":"Yeouido Branch",
+            "place": "Yeongdeungpo-gu, Seoul",
+			"Phone": "010-2537-7605",
+            "types": ",dyfi,eq-location-map,general-link,geoserve,historical-moment-tensor-map,historical-seismicity-map,nearby-cities,origin,p-wave-travel-times,phase-data,scitech-link,tectonic-summary,"
+        },
+        "geometry": {"type": "Point", "coordinates": [ 126.92454666390898,37.522418805618564, 111.16]},
+        "id": "usc000csx3"
+    },
+		{
+            "type": "place2",
+            "properties": {
+                "name":"Gangnam Station Branch",
+				"place": "Gangnam-gu, Seoul",
+				"Phone": "010-2950-0192",
+                "types": ",dyfi,eq-location-map,geoserve,historical-moment-tensor-map,historical-seismicity-map,losspager,nearby-cities,origin,p-wave-travel-times,phase-data,scitech-link,shakemap,tectonic-summary,"
+            },
+            "geometry": {"type": "Point", "coordinates": [ 127.02868968181002,37.49698153744733, 544.19]},
+            "id": "usc000csw5"
+        },
+			{
+            "type": "Feature",
+            "properties": {
+				"name":"Namseong Station Branch",
+				"place": "Dongjak-gu, Seoul",
+				"Phone": "010-3760-1289",
+				"types": ",dyfi,eq-location-map,geoserve,historical-moment-tensor-map,historical-seismicity-map,losspager,nearby-cities,origin,p-wave-travel-times,phase-data,scitech-link,shakemap,tectonic-summary,"
+            },
+            "geometry": {"type": "Point", "coordinates": [ 126.97068187918924,37.48460604510929 , 544.19]},
+            "id": "usc000csw5"
+        },
+				{
+            "type": "Feature",
+            "properties": {
+                "name":"Lotte Tower Branch",
+				"place": "Songpa District, Seoul",
+				"Phone": "010-1923-1234",
+                "types": ",dyfi,eq-location-map,geoserve,historical-moment-tensor-map,historical-seismicity-map,losspager,nearby-cities,origin,p-wave-travel-times,phase-data,scitech-link,shakemap,tectonic-summary,"
+            },
+            "geometry": {"type": "Point", "coordinates": [127.10622676755303 ,37.505192725530605,  544.19]},
+            "id": "usc000csw5"
+        },
+				{
+            "type": "Feature",
+            "properties": {
+                "name":"Chungbuk University Branch",
+				"place": "Bokdae-dong, Cheongju",
+				"Phone": "010-3049-1234",
+                "types": ",dyfi,eq-location-map,geoserve,historical-moment-tensor-map,historical-seismicity-map,losspager,nearby-cities,origin,p-wave-travel-times,phase-data,scitech-link,shakemap,tectonic-summary,"
+            },
+            "geometry": {"type": "Point", "coordinates": [127.449509,36.626054,  544.19]},
+            "id": "usc000csw5"
+        },
+				{
+            "type": "Feature",
+            "properties": {
+                "name":"Sejong City Branch",
+				"place": "Sejong City",
+				"Phone": "010-3123-9501",
+                "types": ",dyfi,eq-location-map,geoserve,historical-moment-tensor-map,historical-seismicity-map,losspager,nearby-cities,origin,p-wave-travel-times,phase-data,scitech-link,shakemap,tectonic-summary,"
+            },
+            "geometry": {"type": "Point", "coordinates": [127.26537640278502 ,36.48327926830978 ,  544.19]},
+            "id": "usc000csw5"
+        },
+				{
+            "type": "Feature",
+            "properties": {
+                "name":"Daejeon Station Branch",
+				"place": "Dunsan-dong, Daejeon",
+				"Phone": "010-9999-9999",
+                "types": ",dyfi,eq-location-map,geoserve,historical-moment-tensor-map,historical-seismicity-map,losspager,nearby-cities,origin,p-wave-travel-times,phase-data,scitech-link,shakemap,tectonic-summary,"
+            },
+            "geometry": {"type": "Point", "coordinates": [127.38239079568028 , 36.35079682134209,  544.19]},
+            "id": "usc000csw5"
+        },
+				{
+            "type": "Feature",
+            "properties": {
+                "name":"Daegu City Hall",
+				"place": "Jung-gu, Daegu",
+				"Phone": "010-3049-1234",
+                "types": ",dyfi,eq-location-map,geoserve,historical-moment-tensor-map,historical-seismicity-map,losspager,nearby-cities,origin,p-wave-travel-times,phase-data,scitech-link,shakemap,tectonic-summary,"
+            },
+            "geometry": {"type": "Point", "coordinates": [128.59270897152055 ,35.86623270491404 ,  544.19]},
+            "id": "usc000csw5"
+        },
+				{
+            "type": "Feature",
+            "properties": {
+                "name":"Haeundae branch",
+				"place": "Gwangalli, Busan",
+				"Phone": "010-3049-1234",
+                "types": ",dyfi,eq-location-map,geoserve,historical-moment-tensor-map,historical-seismicity-map,losspager,nearby-cities,origin,p-wave-travel-times,phase-data,scitech-link,shakemap,tectonic-summary,"
+            },
+            "geometry": {"type": "Point", "coordinates": [129.1176899085877 ,35.15381909140149 ,  544.19]},
+            "id": "usc000csw5"
+        },
+				{
+            "type": "Feature",
+            "properties": {
+                "name":"Gwangju Terminal Branch",
+				"place": "Gwangju City West",
+				"Phone": "010-5555-5555",
+                "types": ",dyfi,eq-location-map,geoserve,historical-moment-tensor-map,historical-seismicity-map,losspager,nearby-cities,origin,p-wave-travel-times,phase-data,scitech-link,shakemap,tectonic-summary,"
+            },
+            "geometry": {"type": "Point", "coordinates": [126.88195054617736  ,35.160015867607505  ,  544.19]},
+            "id": "usc000csw5"
+        },
+		{
+            "type": "Feature",
+            "properties": {
+                "name":"Jeju Airport Branch",
+				"place": "Seogwipo City, Jeju",
+				"Phone": "010-3333-3333",
+                "types": ",dyfi,eq-location-map,geoserve,historical-moment-tensor-map,historical-seismicity-map,losspager,nearby-cities,origin,p-wave-travel-times,phase-data,scitech-link,shakemap,tectonic-summary,"
+            },
+            "geometry": {"type": "Point", "coordinates": [126.4936199763049   ,33.50528782473163 ,  544.19]},
+            "id": "usc000csw5"
+        },
+		{
+            "type": "Feature",
+            "properties": {
+                "name":"Cheorwon Terminal Branch",
+				"place": "Wassuri, Cheorwon",
+				"Phone": "010-4444-4444",
+                "types": ",dyfi,eq-location-map,geoserve,historical-moment-tensor-map,historical-seismicity-map,losspager,nearby-cities,origin,p-wave-travel-times,phase-data,scitech-link,shakemap,tectonic-summary,"
+            },
+            "geometry": {"type": "Point", "coordinates": [127.4381672936743   ,38.23931093480744,  544.19]},
+            "id": "usc000csw5"
+        },
+		{
+            "type": "Feature",
+            "properties": {
+                "name":"Sokcho Terminal Branch",
+				"place": "Choyang-dong, Sokcho",
+				"Phone": "010-2222-1111",
+                "types": ",dyfi,eq-location-map,geoserve,historical-moment-tensor-map,historical-seismicity-map,losspager,nearby-cities,origin,p-wave-travel-times,phase-data,scitech-link,shakemap,tectonic-summary,"
+            },
+            "geometry": {"type": "Point", "coordinates": [128.59104011046773    ,38.21093135942216 ,  544.19]},
+            "id": "usc000csw5"
+        },
+		{
+            "type": "Feature",
+            "properties": {
+                "name":"Suwon Station Branch",
+				"place": "Kwon Seon-dong, Suwon",
+				"Phone": "010-2222-1111",
+                "types": ",dyfi,eq-location-map,geoserve,historical-moment-tensor-map,historical-seismicity-map,losspager,nearby-cities,origin,p-wave-travel-times,phase-data,scitech-link,shakemap,tectonic-summary,"
+            },
+            "geometry": {"type": "Point", "coordinates": [127.02087494051901    ,37.25050317339597  ,  544.19]},
+            "id": "usc000csw5"
+        },
+		
+		{
+            "type": "Feature",
+            "properties": {
+                "name":"Wonju Terminal Branch",
+				"place": "Wonju, Gangwon Province",
+				"Phone": "010-2222-1234",
+                "types": ",dyfi,eq-location-map,geoserve,historical-moment-tensor-map,historical-seismicity-map,losspager,nearby-cities,origin,p-wave-travel-times,phase-data,scitech-link,shakemap,tectonic-summary,"
+            },
+            "geometry": {"type": "Point", "coordinates": [127.92744674028022     ,37.352398524411576  ,  544.19]},
+            "id": "usc000csw5"
+        },
+		{
+            "type": "Feature",
+            "properties": {
+                "name":"Andong Station Branch",
+				"place": "Jung-gu-dong, Andong",
+				"Phone": "010-7777-7777",
+                "types": ",dyfi,eq-location-map,geoserve,historical-moment-tensor-map,historical-seismicity-map,losspager,nearby-cities,origin,p-wave-travel-times,phase-data,scitech-link,shakemap,tectonic-summary,"
+            },
+            "geometry": {"type": "Point", "coordinates": [127.92744674028022     ,37.352398524411576  ,  544.19]},
+            "id": "usc000csw5"
+        },
+				{
+            "type": "Feature",
+            "properties": {
+				"name":"Chungdae Hospital Branch",
+				"place": "Bokdae-dong, Cheongju",
+				"Phone": "010-5632-8745",
+                "types": ",dyfi,eq-location-map,geoserve,historical-moment-tensor-map,historical-seismicity-map,losspager,nearby-cities,origin,p-wave-travel-times,phase-data,scitech-link,shakemap,tectonic-summary,"
+            },
+            "geometry": {"type": "Point", "coordinates": [127.462610,36.624434,  544.19]},
+            "id": "usc000csw5"
+        }]
+});
---
+++ burger/burgerHuman/js/jqbar.js
@@ -0,0 +1,112 @@
+﻿/*********************************************
+
+Author : EGrappler.com
+URL    : http://www.egrappler.com
+License: http://www.egrappler.com/license.
+
+*********************************************/
+(function ($) {
+    $.fn.extend({
+        jqbar: function (options) {
+            var settings = $.extend({
+                animationSpeed: 180000,
+                barLength: 500,
+                orientation: 'h',
+                barWidth: 20,
+                barColor: 'red',
+                label: '&nbsp;',
+                value: 100
+            }, options);
+			
+            return this.each(function () {
+
+                var valueLabelHeight = 0;
+                var progressContainer = $(this);
+				//horizon progress bar
+                if (settings.orientation == 'h') {
+
+                    progressContainer.addClass('jqbar horizontal').append('<span class="bar-label"></span><span class="bar-level-wrapper"><span class="bar-level"></span></span><span class="bar-percent"></span>');
+
+                    var progressLabel = progressContainer.find('.bar-label').html(settings.label);
+                    var progressBar = progressContainer.find('.bar-level').attr('data-value', settings.value);
+                    var progressBarWrapper = progressContainer.find('.bar-level-wrapper');
+
+                    progressBar.css({ height: settings.barWidth, width: 0, backgroundColor: settings.barColor });
+
+                    var valueLabel = progressContainer.find('.bar-percent');
+                }
+                else {
+				//vertical progress bar
+                    progressContainer.addClass('jqbar vertical').append('<span class="bar-percent"></span><span class="bar-level-wrapper"><span class="bar-level"></span></span><span class="bar-label"></span>');
+
+                    var progressLabel = progressContainer.find('.bar-label').html(settings.label);
+                    var progressBar = progressContainer.find('.bar-level').attr('data-value', settings.value);
+                    var progressBarWrapper = progressContainer.find('.bar-level-wrapper');
+
+                    progressContainer.css('height', settings.barLength);
+                    progressBar.css({ height: settings.barLength,top: settings.barLength, width: settings.barWidth, backgroundColor: settings.barColor });
+                    progressBarWrapper.css({ height: settings.barLength, width: settings.barWidth });
+
+                    var valueLabel = progressContainer.find('.bar-percent');
+                    valueLabelHeight = parseInt(valueLabel.outerHeight());
+                    valueLabel.css({ top: (settings.barLength - valueLabelHeight) + 'px' });
+                }
+
+                animateProgressBar(progressBar);
+
+                function animateProgressBar(progressBar) {
+					var countdown=Date.parse(sessionStorage.getItem("getTimer_c"));
+					var starttime =Date.parse(sessionStorage.getItem("getTimer_f"));
+					if(countdown && starttime){
+						var now = new Date().getTime();
+						
+						var distance = countdown - now;
+						var stdistance = countdown-starttime;
+						var ratio = (distance / stdistance)*500;
+					}
+                    var level = parseInt(progressBar.attr('data-value'));
+					
+                    if (level > 100) {
+                        level = 100;
+                        alert('max value cannot exceed 100 percent');
+                    }
+                    var w = settings.barLength * level / 100;
+					//horizon progress bar
+                    if (settings.orientation == 'h') {
+                        progressBar.animate({ width: w }, {
+                            duration: distance,
+                            step: function (currentWidth) {
+                                var percent = parseInt(currentWidth / settings.barLength * 100);
+                                if (isNaN(percent))
+                                    percent = 0;
+                                progressContainer.find('.bar-percent').html(percent + '%');
+                            }
+                        });
+                    }
+                    else {
+					//vertical progress bar
+						progressBar.animate({top:ratio},{
+                            duration: 0,
+                            step: function (currentValue) {
+								
+                            }
+                        });
+
+                        var h = (settings.barLength - settings.barLength * level / 100);
+                        progressBar.animate({ top: h  }, {
+                            duration: distance,
+                            step: function (currentValue) {
+                             
+                            }
+                        });
+                        
+
+                    }
+					
+                }
+
+            });
+        }
+    });
+
+})(jQuery);
